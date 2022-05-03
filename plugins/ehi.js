060b3b8b311ff9aa4abb782e22cf2f99393d6f37
Dark-Max-Alpha <100671455+Dark-Max-Alpha@users.noreply.github.com>
2022-05-03 03:35:32 -0700

    Added All ehi

--- plugins/ehi.js
+++ plugins/ehi.js
@@ -44,16 +44,72 @@ var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main
 
    }));        
 
-     
+        Alpha.addrex({pattern: 'dzehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/D-Zoom%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'D-Zoom♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+        Alpha.addrex({pattern: 'nfehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/NetFix%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyNetFix♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+               Alpha.addrex({pattern: 'iehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/IMO%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyIMO♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+
+               Alpha.addrex({pattern: 'm0ehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Moditel%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Moditel 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+
+               Alpha.addrex({pattern: 'h0ehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Hutch%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Hutch 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+               Alpha.addrex({pattern: 'd0ehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Dialog%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Dialog 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+               Alpha.addrex({pattern: 'a0ehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Aritel%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Aritel 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+        }));
+
+
 
   Alpha.addrex({pattern: 'allehi', fromMe: false,dontaddrexList:true}, (async (message, match) => {
 
 var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Whatsapp%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
 
         await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyWhatsapp♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
-
-       
-
+    
 var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/YouTube%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
 
         await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyFacebook♻️SUCCESS♻️.ehi' , mimetype: 'application/octet-stream' ,quoted: message.data})
@@ -66,9 +122,37 @@ var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main
 
         await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyYouTube♻️SUCCESS♻️.ehi' , mimetype: 'application/octet-stream' ,quoted: message.data})
 
-  
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/D-Zoom%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
 
-        
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'D-Zoom♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/NetFix%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyNetFix♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+  
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/IMO%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'AnyIMO♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Moditel%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Moditel 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+ var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Hutch%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Hutch 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})  
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Dialog%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Dialog 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+var document = await axios.get ('https://github.com/Dark-Max-Alpha/ehi/blob/main/Aritel%200%E2%99%BB%EF%B8%8FSUCCESS%E2%99%BB%EF%B8%8F.ehi?raw=true', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (document.data), MessageType.document, { filename : 'Aritel 0♻️SUCCESS♻️.ehi' , mimetype:  'application/octet-stream' ,quoted: message.data})
+
+
+     
 
 }));
 
